45d620452d27a18e85bcb647137bb97cab14144f
Rique <henriquegoncm@gmail.com>
2026-03-10 16:09:07 -0300

    ts is my first API

---
+++ routes/public.js
@@ -0,0 +1,60 @@
+import express from 'express'
+import { PrismaClient } from '@prisma/client'
+import bcrypt, { genSalt } from 'bcrypt'
+
+const prisma = new PrismaClient()
+const router = express.Router()
+
+router.post('/registrar', async (req, res) => {
+    const user = req.body
+
+    const salt = await bcrypt.genSalt(10)
+    const hashPassword = await bcrypt.hash(user.password, salt)
+
+    try {
+        const newUser = await prisma.user.create ( {
+        data: {
+            name: user.name,
+            age: user.age,
+            email: user.email,
+            password: hashPassword
+        }
+        })
+    }catch (err) {
+        console.error(err)
+
+        return res.status(500).json({message: "Servidor instável, tente novamente mais tarde"})
+    }
+
+    res.status(200).json({message:`Funcionou essa bosta, ${req.body}`}) 
+})
+
+router.get('/listar', async (req, res) => {
+    const consult = await prisma.user.findMany()
+
+    console.log(JSON.stringify(consult, null, 2))
+
+    res.json(consult)
+})
+
+router.delete('/deletar/:id', async (req, res) => {
+        const { id } = req.params
+
+        try {
+            const userDeleted = await prisma.user.delete ({
+                where: {
+                    id: id
+                }
+            })
+
+            res.json(userDeleted)
+
+            } catch (err) {
+                console.error(err)
+
+                res.status(500).json({message: "Servidor instável, tente novamente mais tarde"})
+            }
+        }
+    )
+
+export default router
---
+++ server.js
@@ -0,0 +1,10 @@
+import 'dotenv/config'
+import express from 'express'
+import publicRouter from './routes/public.js'
+
+const app = express()
+app.use(express.json())
+
+app.use('/', publicRouter)
+
+app.listen(3000, () => {console.log("Desgraça sô")})
