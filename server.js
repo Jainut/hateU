8676121c3f7bd60009e450edeb1bfa71d499541c
Rique <henriquegoncm@gmail.com>
2026-03-12 19:39:20 -0300

    updated README

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ import publicRouter from './routes/public.js'
 const app = express()
 app.use(express.json())
 
-app.use('/', publicRouter)
+app.use('/', publicRouter, () => {
+    res.send("Rodando essa bomba 🔥🔥🔥")
+})
 
 app.listen(3000, () => {console.log("Desgraça sô")})
